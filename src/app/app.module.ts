2841132eb870b8456e230513097b4970ea64b2f1
raghavpatidar <raghavpatidar997764@gmail.com>
2023-01-23 17:26:56 +0530

    task completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeroComponent } from './hero/hero.component';
+import { MainComponent } from './main/main.component';
+import { BannerComponent } from './banner/banner.component';
+import { ExtraComponent } from './extra/extra.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    HeroComponent,
+    MainComponent,
+    BannerComponent,
+    ExtraComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
